2df3fe431df21bbd4493b431b8fd773f69cd25f8
dmit-s <dwhatly@gmail.com>
2023-11-27 16:42:48 +0300

    design

---
+++ app.js
@@ -0,0 +1,13 @@
+import DateFormInstance from "./src/components/DateForm/DateForm.js";
+
+class App{
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        DateFormInstance.init();
+    }
+}
+
+new App();
---
+++ src/components/DateForm/DateForm.js
@@ -0,0 +1,62 @@
+class DateForm{
+    static days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    static months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+    constructor(){
+        this.inputEL = document.getElementById('date-input');
+        this.labelEl = document.getElementById('date-label');
+    }
+
+    init(){
+        this.attachEvents();
+        this.changeDate(this.inputEL);
+    }
+
+    attachEvents(){
+        this.inputEL.addEventListener('change', this.changeDate.bind(this))
+        this.labelEl.addEventListener('click', () => {
+            this.inputEL.showPicker();
+        });
+    }
+
+    formatDate(date){
+        const dd = date.getDate();
+        const mm = date.getMonth() + 1;
+        const yyyy = date.getFullYear();
+
+        return `${yyyy}-${mm}-${dd}`
+    }
+
+
+    changeDate(e){
+        let date = new Date(e.target.value);
+        console.log(e.target.checked);
+
+        if(isNaN(date)){
+            date = new Date();
+            this.inputEL.value = this.formatDate(date);
+            
+        }
+
+        const dayWeek = date.getDay();
+        const dayNumber = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+
+        this.labelEl.innerText = `${DateForm.days[dayWeek]}, ${DateForm.months[month]} ${dayNumber}`;
+      
+        // const splited = this.inputEL.value.split('-');
+        // const dateObj = {
+        //     day: splited[2],
+        //     month: splited[1],
+        //     year: splited[0]
+        // }
+
+
+        
+        
+    }
+}
+
+export default new DateForm();
