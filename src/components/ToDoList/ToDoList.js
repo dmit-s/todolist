641a8b472c58d58c68023746703be3f5845a6c81
dmit-s <dwhatly@gmail.com>
2023-11-28 17:26:37 +0300

    upd

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -4,19 +4,17 @@ export class ToDoList {
     this.addTaskInput = document.getElementById("add-input");
     this.addingFormEl = document.getElementById("adding-form");
     this.tasksList = document.getElementById("tasks-list");
-    this.refreshBtn = document.getElementById('refresh-tasks-btn');
- 
+    this.refreshBtn = document.getElementById("refresh-tasks-btn");
   }
 
   init() {
     this.attachEvents();
     this.renderTasks();
-
   }
 
   attachEvents() {
     this.addingFormEl.addEventListener("submit", this.addTask.bind(this));
-    this.refreshBtn.addEventListener('click', this.refresh.bind(this));
+    this.refreshBtn.addEventListener("click", this.refresh.bind(this));
   }
 
   addTask(e) {
@@ -31,12 +29,12 @@ export class ToDoList {
     };
     const dateId = this.inputEL.value.split("-").join("");
     let tasksStorage = JSON.parse(localStorage.getItem("Tasks"));
-    
+
     if (!tasksStorage) {
       tasksStorage = {
         [dateId]: [task],
-      }
-    } else if(tasksStorage[dateId]){
+      };
+    } else if (tasksStorage[dateId]) {
       tasksStorage[dateId].push(task);
     } else {
       tasksStorage[dateId] = [task];
@@ -47,45 +45,60 @@ export class ToDoList {
     e.target.reset();
   }
 
-  removeTask(e){
-    const btnEl = e.target.closest('#trash-btn');
+  removeTask(e) {
+    const btnEl = e.target.closest("#trash-btn");
     const taskItemEl = btnEl.parentElement.parentElement;
     const currentDateId = this.inputEL.value.split("-").join("");
-    const tasksStorage = JSON.parse(localStorage.getItem('Tasks'));
+    const tasksStorage = JSON.parse(localStorage.getItem("Tasks"));
 
-  
-    const filteredTasks = tasksStorage[currentDateId].filter(t => t.id !== taskItemEl.dataset.id);
+    const filteredTasks = tasksStorage[currentDateId].filter(
+      (t) => t.id !== taskItemEl.dataset.id
+    );
     tasksStorage[currentDateId] = filteredTasks;
-    localStorage.setItem('Tasks', JSON.stringify(tasksStorage));
+    localStorage.setItem("Tasks", JSON.stringify(tasksStorage));
     taskItemEl.remove();
   }
 
-  completeTask(e){
-    const btnEl = e.target.closest('#complete-btn');
+  completeTask(e) {
+    const btnEl = e.target.closest("#complete-btn");
     const taskItemEl = btnEl.parentElement.parentElement;
     const currentDateId = this.inputEL.value.split("-").join("");
-    const tasksStorage = JSON.parse(localStorage.getItem('Tasks'));
+    const tasksStorage = JSON.parse(localStorage.getItem("Tasks"));
 
-    const findTask = tasksStorage[currentDateId].find(t => t.id == taskItemEl.dataset.id);
+    const findTask = tasksStorage[currentDateId].find(
+      (t) => t.id == taskItemEl.dataset.id
+    );
 
-    if(findTask.completed){
+    if (findTask.completed) {
       findTask.completed = false;
-      taskItemEl.classList.remove('checked');
-    } else{
+      taskItemEl.classList.remove("checked");
+    } else {
       findTask.completed = true;
-      taskItemEl.classList.add('checked');
+      taskItemEl.classList.add("checked");
     }
 
+    localStorage.setItem("Tasks", JSON.stringify(tasksStorage));
+  }
+
+  taskEdit(e) {
+    const target = e.target;
+    const taskItemEl = e.target.parentElement;
+    const tasksStorage = JSON.parse(localStorage.getItem('Tasks'));
+    const dateId = this.inputEL.value.split('-').join('');
+    const findTask = tasksStorage[dateId].find(t => t.id == taskItemEl.dataset.id);
+    findTask.text = target.value;
     localStorage.setItem('Tasks', JSON.stringify(tasksStorage));
   }
 
-  createMarkup(tasks){
-    return tasks.map(t => {
-      const {id, text, completed} = t;
-    
+  createMarkup(tasks) {
+    return tasks.map((t) => {
+      const { id, text, completed } = t;
+
       return `
-        <div id="task-item"  data-id=${id} class="tasks-list__item ${completed && 'checked'}">
-          <input class="tasks-list__item-input" type="text" value=${text} />
+        <div id="task-item"  data-id=${id} class="tasks-list__item ${
+        completed && "checked"
+      }">
+          <input id="task-input" class="tasks-list__item-input" type="text" value=${text} />
           <div class="tasks-list__item-buttons-wrapper">
             <button id="complete-btn" class="tasks-list__item-complete-btn">
               <i class="fa-solid fa-circle-check"></i>
@@ -97,43 +110,53 @@ export class ToDoList {
           </div>
         </div>
       `;
-    })
+    });
   }
 
-  attachTasksEvents(){
-    const completeBtns = document.querySelectorAll('#complete-btn');
-    const trashBtns = document.querySelectorAll('#trash-btn');
+  attachTasksEvents() {
+    const completeBtns = document.querySelectorAll("#complete-btn");
+    const trashBtns = document.querySelectorAll("#trash-btn");
+    const tasksInputs = document.querySelectorAll("#task-input");
 
-    trashBtns.forEach(btn => btn.addEventListener('click', this.removeTask.bind(this)));
-    completeBtns.forEach(btn => btn.addEventListener('click', this.completeTask.bind(this)))
+    tasksInputs.forEach((item) =>
+      item.addEventListener("blur", this.taskEdit.bind(this))
+    );
+    tasksInputs.forEach((item) =>
+    item.addEventListener("keyup", this.taskEdit.bind(this))
+  );
+    trashBtns.forEach((btn) =>
+      btn.addEventListener("click", this.removeTask.bind(this))
+    );
+    completeBtns.forEach((btn) =>
+      btn.addEventListener("click", this.completeTask.bind(this))
+    );
   }
 
-  renderTasks(){
+  renderTasks() {
     this.clearTasksList();
-    const tasks = JSON.parse(localStorage.getItem('Tasks'));
-    if(!tasks) return;
+    const tasks = JSON.parse(localStorage.getItem("Tasks"));
+    if (!tasks) return;
     const currentDate = this.inputEL.value; //*
-    const todayTasks = tasks[currentDate.split('-').join('')];
+    const todayTasks = tasks[currentDate.split("-").join("")];
 
-
-    if(!todayTasks) {
-      console.log('not found');
+    if (!todayTasks) {
+      console.log("not found");
       return;
-    };
+    }
 
     const markup = this.createMarkup(todayTasks);
-    this.tasksList.insertAdjacentHTML('beforeend', markup.join(''));
+    this.tasksList.insertAdjacentHTML("beforeend", markup.join(""));
 
     this.attachTasksEvents();
   }
 
-  clearTasksList(){
-    const listEl = document.getElementById('tasks-list');
+  clearTasksList() {
+    const listEl = document.getElementById("tasks-list");
     listEl.innerHTML = ``;
   }
 
-  refresh(){
-    localStorage.removeItem('Tasks');
+  refresh() {
+    localStorage.removeItem("Tasks");
     this.renderTasks();
   }
 }
